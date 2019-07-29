a97b78098c7b6d889618f4f7b7a862a7a46c70aa
Erik Moqvist <erik.moqvist@gmail.com>
2019-07-29 10:27:19 +0200

    asd

--- soundid/__init__.py
+++ soundid/__init__.py
@@ -1,5 +1,9 @@
 import glob
 from tinytag import TinyTag
+from skimage.feature import peak_local_max
+from pydub import AudioSegment
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plot
 
 from .version import __version__
 
@@ -28,8 +32,61 @@ class Database:
 
     @property
     def sounds(self):
+        """A list of all sounds in the database.
+
+        """
+        
         return self._sounds
 
     @property
     def number_of_sounds(self):
+        """Number of sounds in the database.
+
+        """
+        
         return len(self._sounds)
+
+    def identify_file(self, soundfile):
+        """Identify given sound file.
+
+        Returns the identified sound, or ``None`` if the sound was not
+        identified.
+
+        """
+
+        sound = AudioSegment.from_file(soundfile)
+        samples = sound.get_array_of_samples()
+        fingerprint = create_fingerprint(samples[0::sound.channels],
+                                         sound.frame_rate)
+
+        return self._sounds[0]
+
+
+def create_fingerprint(samples, sampling_rate):
+    """Create a fingerprint of given samples.
+
+    """
+
+    spectrum, freqs, times = mlab.specgram(samples,
+                                           NFFT=4096,
+                                           Fs=sampling_rate,
+                                           window=mlab.window_hanning,
+                                           noverlap=2048)
+    coordinates = peak_local_max(spectrum, min_distance=5)
+
+    plot.specgram(samples,
+                  NFFT=4096,
+                  Fs=sampling_rate,
+                  window=mlab.window_hanning,
+                  noverlap=2048)
+    plot.xlabel('Time')
+    plot.ylabel('Frequency')
+    plot.show()   
+
+    fingerprint = []
+    
+    for x, y in coordinates:
+        fingerprint.append((int(1000 * times[y]), int(freqs[x])))
+
+    for t, f in sorted(fingerprint):
+        print(t, f)
--- tests/test_soundid.py
+++ tests/test_soundid.py
@@ -2,7 +2,7 @@ import unittest
 import soundid
 
 
-class SoundIdTest(unittest.TestCase):
+class DatabaseTest(unittest.TestCase):
 
     def test_create_database(self):
         database = soundid.Database('test_create_database.sid',
@@ -11,8 +11,22 @@ class SoundIdTest(unittest.TestCase):
                                         'tests/files/bar.mp3'
                                     ])
         self.assertEqual(database.number_of_sounds, 2)
+        self.assertEqual(database.sounds[0].artist, 'Foo Band')
+        self.assertEqual(database.sounds[0].title, 'Foo Song')
         self.assertEqual(database.sounds[0].name, 'Foo Band - Foo Song')
         self.assertEqual(database.sounds[1].name, 'Bar Artist - Bar Song')
+
+    def test_identify_file(self):
+        database = soundid.Database('test_identify_file.sid',
+                                    ['tests/files/foo.mp3'])
+        sound = database.identify_file('tests/files/foo.mp3')
+        self.assertEqual(sound.title, 'Foo Song')
+
+    def test_identify_file_sine_440hz(self):
+        database = soundid.Database('test_identify_file.sid',
+                                    ['tests/files/sine-440hz.mp3'])
+        sound = database.identify_file('tests/files/sine-440hz.mp3')
+        self.assertEqual(sound.title, '')
 
 
 if __name__ == '__main__':
